902698aa41c2a116c55843f55e7fdf45cd938f58
jean-jonathan <jean-jonathan.koffi@etud.univ-pau.fr>
2023-02-09 23:32:24 +0100

    fonctionnalité pour marquer une tâche comme terminée. On ajoute une méthode terminerTache dans le composant Tache puis ajout un bouton "Terminer" dans le template du composant Tache pour appeler cette methode

--- src/app/tache/tache.component.ts
+++ src/app/tache/tache.component.ts
@@ -1,16 +1,30 @@
 import { Component } from '@angular/core';
 /*Composant stockant les taches*/
 @Component({
-  selector: 'app-composant1',
-  templateUrl: './composant1.component.html',
-  styleUrls: ['./composant1.component.css']
-
+  selector: 'app-tache',
+  //templateUrl: './tache.component.html',
+  //styleUrls: ['./tache.component.css']
+  template: `
+    <ul>
+      <li *ngFor="let tache of taches; index as i">
+        {{ tache.nom }}: {{ tache.description }}
+        <button (click)="terminerTache(i)">Terminer</button>
+      </li>
+    </ul>
+  `,
 })
+
 export class TacheComponent {
   taches = [
     { nom: 'Tâche 1', description: 'Description de la tâche 1' },
     { nom: 'Tâche 2', description: 'Description de la tâche 2' },
     // ...
   ];
+  //Definition de la méthode "terminerTache"
+  terminerTache(index: number) {
+    this.taches.splice(index, 1);
+  }
+
 }
+
 
