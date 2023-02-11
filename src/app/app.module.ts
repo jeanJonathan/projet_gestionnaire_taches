4900fefbdd5c7fc496e626268b87ec99c83fe9b9
jean-jonathan <jean-jonathan.koffi@etud.univ-pau.fr>
2023-02-11 12:56:09 +0100

    implementation d un formulaire adresse en utilisant angular materiel pour test

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,22 +9,35 @@ import { TacheListeComponent } from './tache-liste/tache-liste.component';
 import { TacheComponent } from './tache/tache.component';
 import { AjouterTacheComponent } from './ajouter-tache/ajouter-tache.component';
 //On importe la bibliotheque Angular Forms pour pouvoir utiliser la directive ngSubmit dans le Composant ajouterTache
-import {FormsModule} from "@angular/forms";
+import {FormsModule, ReactiveFormsModule} from "@angular/forms";
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { FormAdressComponent } from './form-adress/form-adress.component';
+import { MatInputModule } from '@angular/material/input';
+import { MatButtonModule } from '@angular/material/button';
+import { MatSelectModule } from '@angular/material/select';
+import { MatRadioModule } from '@angular/material/radio';
+import { MatCardModule } from '@angular/material/card';
 
 @NgModule({
   declarations: [
     AppComponent,
     TacheListeComponent,
     TacheComponent,
-    AjouterTacheComponent
+    AjouterTacheComponent,
+    FormAdressComponent
   ],
   imports: [
     MatSlideToggleModule,
     BrowserModule,
     AppRoutingModule,
     FormsModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    MatInputModule,
+    MatButtonModule,
+    MatSelectModule,
+    MatRadioModule,
+    MatCardModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
